74ab208b4ed1f9fb5cd16dc5c7e5c15b93efec04
Qui Nguyen <1322636+qui@users.noreply.github.com>
2019-08-22 16:01:02 -0700

    Release 0.12.0-yelp1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hacheck",
-    version="0.11.0",
+    version="0.12.0",
     author="James Brown",
     author_email="jbrown@uber.com",
     url="https://github.com/uber/hacheck",
