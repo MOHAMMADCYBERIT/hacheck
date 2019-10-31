9bf03d314970d32bb0fbee4e26c21ac00b09e2ed
Mingqi Zhu <mingqiz@yelp.com>
2019-10-31 10:59:57 -0700

    Release 0.13.0-yelp1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hacheck",
-    version="0.12.0",
+    version="0.13.0",
     author="James Brown",
     author_email="jbrown@uber.com",
     url="https://github.com/uber/hacheck",
