09bc0ac31e81d9d4d8779f1c80dbd05fbd53d1a0
Stuart Elston <selston@yelp.com>
2019-11-18 17:35:35 -0800

    Release 0.16.0-yelp1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hacheck",
-    version="0.15.0",
+    version="0.16.0",
     author="James Brown",
     author_email="jbrown@uber.com",
     url="https://github.com/uber/hacheck",
